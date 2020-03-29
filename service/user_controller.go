760a6849f157d1e728a15ac77abcb0c50c889f14
Zed <kastnerorz@gmail.com>
2020-03-29 15:40:41 +0800

    feat: add username uniqueness check in `CreateUser`

--- service/user_controller.go
+++ service/user_controller.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net/http"
 )
@@ -15,7 +16,7 @@ func CreateUser(c *gin.Context) {
 	var user User
 	err := c.BindJSON(&user)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"code": -1, "msg": "内部错误！"})
+		c.JSON(http.StatusBadRequest, gin.H{"code": -1, "msg": "（-1）内部错误！"})
 		log.Println(err)
 		return
 	}
@@ -25,27 +26,40 @@ func CreateUser(c *gin.Context) {
 		return
 	}
 
+	mongoCtx, collection := GetMongoContext("users")
+	var res User
+	err = collection.FindOne(mongoCtx, bson.M{"username": user.Username}).Decode(&res)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"code": -3, "msg": "（-3）内部错误！"})
+		log.Println(err)
+		return
+	}
+	if res.Username == user.Username {
+		c.JSON(http.StatusBadRequest, gin.H{"code": -4, "msg": "用户名已被使用！"})
+		return
+	}
+
 	if user.Password == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"code": -3, "msg": "密码不能为空！"})
+		c.JSON(http.StatusBadRequest, gin.H{"code": -5, "msg": "密码不能为空！"})
 		log.Println(err)
 		return
 	}
 
 	if user.Nickname == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"code": -4, "msg": "昵称不能为空！"})
+		c.JSON(http.StatusBadRequest, gin.H{"code": -6, "msg": "昵称不能为空！"})
 		log.Println(err)
 		return
 	}
 
 	if user.SwitchFriendCode == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"code": -5, "msg": "好友编号不能为空！"})
+		c.JSON(http.StatusBadRequest, gin.H{"code": -7, "msg": "好友编号不能为空！"})
 		log.Println(err)
 		return
 	}
 
 	h := sha256.New()
 	h.Write([]byte(user.Password))
-	mongoCtx, collection := GetMongoContext("users")
+	mongoCtx, collection = GetMongoContext("users")
 	_, err = collection.InsertOne(mongoCtx, bson.M{
 		"username":         user.Username,
 		"password":         hex.EncodeToString(h.Sum(nil)),
@@ -53,7 +67,7 @@ func CreateUser(c *gin.Context) {
 		"switchFriendCode": user.SwitchFriendCode,
 	})
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"code": -3, "msg": "Error while inserting into database."})
+		c.JSON(http.StatusInternalServerError, gin.H{"code": -8, "msg": "（-8）内部错误！"})
 		log.Println(err)
 		return
 	}
@@ -65,16 +79,18 @@ func GetUser(c *gin.Context) {
 	mongoCtx, collection := GetMongoContext("users")
 	var res User
 	objectId, _ := primitive.ObjectIDFromHex(c.Param("id"))
-	err := collection.FindOne(mongoCtx, bson.M{"_id": objectId}).Decode(&res)
+	opt := options.FindOne()
+	opt.SetProjection(bson.D{
+		{"_id", 1},
+		{"username", 1},
+		{"nickname", 1},
+		{"switchFriendCode", 1},
+	})
+	err := collection.FindOne(mongoCtx, bson.M{"_id": objectId}, opt).Decode(&res)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"code": -1, "msg": "用户获取失败."})
+		c.JSON(http.StatusInternalServerError, gin.H{"code": -1, "msg": "（-1）内部错误"})
 		log.Println(err)
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{
-		"id":               res.ID,
-		"username":         res.Username,
-		"nickname":         res.Nickname,
-		"switchFriendCode": res.SwitchFriendCode,
-	})
+	c.JSON(http.StatusOK, res)
 }
