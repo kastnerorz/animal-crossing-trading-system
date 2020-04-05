edf4f9ea57992380cc89002f29ec616e4fe9d09c
kelrvins <k.elrvins@gmail.com>
2020-04-05 21:27:05 +0800

    :bug: 根据当前日期限制跳转

--- app/middleware/curDay.js
+++ app/middleware/curDay.js
@@ -1,15 +1,29 @@
 export default function({ store, route, redirect }) {
-  const cDate = new Date()
-  // cDate.setDate(cDate.getDate()+2);
-  const cDay = cDate.getDay()
-  const cHour = cDate.getHours()
-  // if (route.name === 'index') {
-  //   if (cDay !== 0 && cHour < 12) {
-  //     redirect('/takeRest?type=buy')
-  //   }
-  // } else if (route.name === 'sell') {
-  //   if (cDay === 0) {
-  //     redirect('/takeRest?type=sell')
-  //   }
-  // }
+  const cDate = new Date();
+  if (process.env.NODE_ENV === "development") {
+    cDate.setDate(cDate.getDate() + 2);
+  }
+  const cDay = cDate.getDay();
+  const cHour = cDate.getHours();
+  if (route.name === "sell") {
+    if (cDay === 0) {
+      redirect("/");
+    }
+  } else if (route.name === "index") {
+    if (cDay !== 0) {
+      redirect("/sell");
+    } else if (cHour > 11) {
+      redirect("/takeRest");
+    }
+  } else if (route.name === "application") {
+    if (cDay === 0 && cHour > 11) {
+      redirect("/takeRest");
+    }
+  } else if (route.name === "takeRest") {
+    if (cDay !== 0) {
+      redirect("/sell");
+    } else if (cHour < 12) {
+      redirect("/");
+    }
+  }
 }
